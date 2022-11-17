1527039f8f2c03dbfb400b44b5f68e7f782f0f87
Joyce <joyceyukang@gmail.com>
2022-11-17 15:20:28 -0800

    Add Users table migration and seeds

---
+++ backend/db/models/user.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
+          }
+        }
+      }
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      allowNull: false,
+      type: DataTypes.STRING.BINARY,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
