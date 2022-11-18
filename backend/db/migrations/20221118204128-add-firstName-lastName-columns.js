b63266a219f5005bf5df47b360df4389881497c6
Joyce <joyceyukang@gmail.com>
2022-11-18 15:30:21 -0800

    added options object

--- backend/db/migrations/20221118204128-add-firstName-lastName-columns.js
+++ backend/db/migrations/20221118204128-add-firstName-lastName-columns.js
@@ -1,6 +1,11 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn(
@@ -9,18 +14,19 @@ module.exports = {
       {
         allowNull: false,
         type: Sequelize.STRING
-      }),
+      }, options),
       await queryInterface.addColumn(
         'Users',
         'lastName',
         {
           allowNull: false,
           type: Sequelize.STRING
-        })
+        },
+        options)
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName'),
-      await queryInterface.removeColumn('Users', 'lastName')
+    await queryInterface.removeColumn('Users', 'firstName', options),
+      await queryInterface.removeColumn('Users', 'lastName', options)
   }
 };
