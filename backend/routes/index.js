a48961a0a46ce02d185d988bffc0623c41da3641
Joyce <joyceyukang@gmail.com>
2022-12-09 13:30:08 -0800

    all of phase 0 including the csrf tokens frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.status(201).json({});
+    return res.json({});
   });
 }
 
